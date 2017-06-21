cf14408c2bccb9d76297b02e51d9d4f84c6b47e9
fishca <sergrud@mail.ru>
2017-06-21 11:27:12 +0300

    Подправил get_file_name_for_record, границу цикла

--- src/ctool1cd/Class_1CD.cpp
+++ src/ctool1cd/Class_1CD.cpp
@@ -6871,8 +6871,8 @@ String Table::get_file_name_for_record(char* rec)
 	if(num_indexes)
 	{
 		ind = indexes[0];
-		//for(i = 0; i < num_indexes; i++) 
-		for (i = 0; i < ind->num_records; i++) // как должно быть правильно?
+		for(i = 0; i < num_indexes; i++) 
+		
 			if(indexes[i]->is_primary)
 			{
 				ind = indexes[i];
