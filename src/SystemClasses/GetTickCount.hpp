cb8021525ced0d7c7cb7231f9613f8f5552775cd
Sergey Batanov <sergey.batanov@dmpas.ru>
2017-10-24 09:42:57 +0300

    Сам GetTickCount

---
+++ src/SystemClasses/GetTickCount.hpp
@@ -0,0 +1,15 @@
+#ifndef GETTICKCOUNT_HPP_INCLUDED
+#define GETTICKCOUNT_HPP_INCLUDED
+
+namespace System {
+
+namespace Classes {
+
+unsigned long GetTickCount();
+
+}
+
+}
+
+
+#endif // GETTICKCOUNT_HPP_INCLUDED
