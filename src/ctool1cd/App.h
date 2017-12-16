35c7d4cc50caebfd24882d5e7803c0cdb892ba77
Sergey Batanov <sergey.batanov@dmpas.ru>
2017-12-16 12:37:28 +0300

    Форматирование.

--- src/ctool1cd/App.h
+++ src/ctool1cd/App.h
@@ -10,38 +10,38 @@
 
 class App {
 public:
-		App(char **szArglist, int nArgs, Messenger &mess);
-		~App();
+	App(char **szArglist, int nArgs, Messenger &mess);
+	~App();
 
-		int Run();
+	int Run();
 
-		App(const App &app) = delete;
+	App(const App &app) = delete;
 
 private:
-		std::unique_ptr<T_1CD> base1CD;
-		CommandParse comm;
-		Messenger& mess;
+	std::unique_ptr<T_1CD> base1CD;
+	CommandParse comm;
+	Messenger& mess;
 
-		bool ActionOpenBaseNotMonopolyChecked{ false };
-		bool ActionXMLSaveBLOBToFileChecked{ false };
-		bool ActionXMLUnpackBLOBChecked{ true };
+	bool ActionOpenBaseNotMonopolyChecked{ false };
+	bool ActionXMLSaveBLOBToFileChecked{ false };
+	bool ActionXMLUnpackBLOBChecked{ true };
 
-		bool IsTrueString(const String &str) const;
-		void export_all_to_xml(const ParsedCommand& pc);
-		void export_to_xml(const ParsedCommand& pc);
+	bool IsTrueString(const String &str) const;
+	void export_all_to_xml(const ParsedCommand& pc);
+	void export_to_xml(const ParsedCommand& pc);
 
-		void export_to_binary(const ParsedCommand& pc);
-		void import_from_binary(const ParsedCommand& pc);
+	void export_to_binary(const ParsedCommand& pc);
+	void import_from_binary(const ParsedCommand& pc);
 
-		void save_config(const ParsedCommand& pc);
-		void save_configsave(const ParsedCommand& pc);
-		void save_vendors_configs(const ParsedCommand& pc);
-		void save_all_configs(const ParsedCommand& pc);
+	void save_config(const ParsedCommand& pc);
+	void save_configsave(const ParsedCommand& pc);
+	void save_vendors_configs(const ParsedCommand& pc);
+	void save_all_configs(const ParsedCommand& pc);
 
-		void save_depot_config(const ParsedCommand& pc);
-		void save_depot_config_part(const ParsedCommand& pc);
+	void save_depot_config(const ParsedCommand& pc);
+	void save_depot_config_part(const ParsedCommand& pc);
 
-		void find_and_save_lost_objects(const ParsedCommand& pc);
+	void find_and_save_lost_objects(const ParsedCommand& pc);
 
 };
 
