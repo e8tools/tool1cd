64c480d7da38b62d118c3e9353918e5abc101543
Сергѣй Батанов <dmpas@dmpas.ru>
2018-01-12 21:22:30 +0300

    Тесты на сохранение конфигурации.

--- src/tests/tool1cd/test_depotv6.cpp
+++ src/tests/tool1cd/test_depotv6.cpp
@@ -1,5 +1,8 @@
 #include "../catch.hpp"
 #include <Class_1CD.h>
+#include <boost/filesystem.hpp>
+
+using boost::filesystem::path;
 
 TEST_CASE("Работа с хранилищем версии 6", "[tool1cd][Class_1CD][depotv6]")
 {
@@ -12,6 +15,24 @@ TEST_CASE("Работа с хранилищем версии 6", "[tool1cd][Clas
 			bool result = base1CD.is_open();
 			THEN ("получаем 'true'") {
 				REQUIRE( result == true );
+			}
+		}
+	}
+}
+
+TEST_CASE("Сохраняем конфигурацию хранилища 6", "[tool1cd][Class_1CD][depotv6]")
+{
+	GIVEN( "Хранилище tests/depotv6/depot/1cv8ddb.1CD")  {
+		std::string dbpath(CMAKE_SOURCE_DIR);
+		dbpath += "/tests/depotv6/depot/1cv8ddb.1CD";
+
+		path cfpath = boost::filesystem::temp_directory_path() / "tmp.cf";
+
+		T_1CD base1CD(dbpath, nullptr, true);
+		WHEN ("Сохраняем конфигурацию") {
+			base1CD.save_depot_config(cfpath.string());
+			THEN ("получаем выгруженный файл") {
+				REQUIRE( boost::filesystem::exists(cfpath) );
 			}
 		}
 	}
