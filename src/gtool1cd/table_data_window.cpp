e011cfc5c4893b08b7d24c8a9104b8bc04466ecc
Сергѣй Батанов <dmpas@dmpas.ru>
2018-02-15 14:48:13 +0300

    Показ сырой структуры

--- src/gtool1cd/table_data_window.cpp
+++ src/gtool1cd/table_data_window.cpp
@@ -2,6 +2,7 @@
 #include "ui_table_data_window.h"
 #include "table_window.h"
 #include <Table.h>
+#include "skobkatextwindow.h"
 
 TableDataWindow::TableDataWindow(QWidget *parent, Table *table)
     : QMainWindow(parent), table(table),
@@ -21,7 +22,10 @@ TableDataWindow::~TableDataWindow()
 
 void TableDataWindow::on_descriptionButton_clicked()
 {
-
+	SkobkaTextWindow *w = new SkobkaTextWindow(this);
+	w->setText(QString::fromStdString(static_cast<std::string>(table->getdescription())),
+	           QString::fromStdString(static_cast<std::string>(table->getname())));
+	w->show();
 }
 
 void TableDataWindow::on_fieldsButton_clicked()
