b4f82f187d45eef517a5ea12051a9db418c7b7af
Sergey Batanov <sergey.batanov@dmpas.ru>
2018-02-18 00:55:14 +0300

    Тест на выгрузку сохранённой конфигурации.

--- src/tests/tool1cd/test_saveconfig.cpp
+++ src/tests/tool1cd/test_saveconfig.cpp
@@ -12,9 +12,16 @@ TEST_CASE("Сохранение конфигураций в 8.3.8", "[tool1cd][C
 
 		T_1CD base1CD(dbpath, nullptr, true);
 
-		path cfpath = boost::filesystem::temp_directory_path() / boost::filesystem::unique_path();
-
+		WHEN( "Выгружаем конфигурацию" ) {
+			path cfpath = boost::filesystem::temp_directory_path() / boost::filesystem::unique_path();
+			base1CD.save_configsave(cfpath);
+			THEN( "Получаем файл" ) {
+				REQUIRE(boost::filesystem::exists(cfpath));
+			}
+			boost::filesystem::remove(cfpath);
+		}
 		WHEN( "Выгружаем конфигурацию базы данных" ) {
+			path cfpath = boost::filesystem::temp_directory_path() / boost::filesystem::unique_path();
 			base1CD.save_config(cfpath);
 			THEN( "Получаем файл" ) {
 				REQUIRE(boost::filesystem::exists(cfpath));
