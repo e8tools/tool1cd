69eb3bd7bc75bae456991a5e07427f315eb032da
Alexander Andreev <andreevlex.as@gmail.com>
2018-02-18 17:14:10 +0300

    Добавлены тесты для выгрузки конфигурации поставщика

--- src/tests/tool1cd/test_saveconfig.cpp
+++ src/tests/tool1cd/test_saveconfig.cpp
@@ -30,4 +30,32 @@ TEST_CASE("Сохранение конфигураций в 8.3.8", "[tool1cd][C
 		}
 
 	}
+
+	GIVEN("База tests/db838/db02/1Cv8.1CD") {
+		std::string dbpath(CMAKE_SOURCE_DIR);
+		dbpath += "/tests/db838/db02/1Cv8.1CD";
+
+		T_1CD base1CD(dbpath, nullptr, true);
+
+		WHEN("Выгружаем конфигурацию") {
+			path cfpath = boost::filesystem::temp_directory_path() / boost::filesystem::unique_path();
+			base1CD.save_configsave(cfpath);
+			THEN("Получаем файл") {
+				REQUIRE(boost::filesystem::exists(cfpath));
+			}
+			boost::filesystem::remove(cfpath);
+		}
+
+		WHEN("Выгружаем конфигурацию поставщика") {
+			path cfpath = boost::filesystem::temp_directory_path() / boost::filesystem::unique_path();
+			auto supplier_config = base1CD.supplier_configs().front();
+			supplier_config->save_to_file(cfpath);
+
+			THEN("Получаем файл") {
+				REQUIRE(boost::filesystem::exists(cfpath));
+			}
+			boost::filesystem::remove(cfpath);
+		}
+
+	}
 }
